061073a3ee2fbed236cead879f12525833b5ff45
Kenneth Jones <kennethjones1991@outlook.com>
2020-04-07 20:34:15 -0600

    Set up color model

---
+++ Rainbow/Model/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Rainbow
+//
+//  Created by Kenneth Jones on 4/7/20.
+//  Copyright © 2020 Kenneth Jones. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    var colorName: String
+    var color: UIColor
+}
